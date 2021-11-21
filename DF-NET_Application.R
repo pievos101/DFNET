b3a6be71bd26185d68b3ae66dfe4e3591ba18ef6
pievos101 <bastianxpfeifer@gmail.com>
2021-11-21 22:17:04 +0100

    pre-processing + application tests

--- DF-NET_Application.R
+++ DF-NET_Application.R
@@ -5,21 +5,21 @@ library(pROC)
 library(DFNET)
 
 ## PPI
-#PPI      <- read.table("~/LinkedOmics/BRCA/BRCA_PPI.txt")
-PPI      <- read.table("~/LinkedOmics/KIRC/KIDNEY_PPI.txt")
+PPI      <- read.table("~/LinkedOmics/BRCA/BRCA_PPI.txt")
+#PPI      <- read.table("~/LinkedOmics/KIRC/KIDNEY_PPI.txt")
 
 ## Features
-#mRNA     <- read.table("~/LinkedOmics/BRCA/BRCA_mRNA_FEATURES.txt")
-mRNA     <- read.table("~/LinkedOmics/KIRC/KIDNEY_mRNA_FEATURES.txt")
+mRNA     <- read.table("~/LinkedOmics/BRCA/BRCA_mRNA_FEATURES.txt")
+#mRNA     <- read.table("~/LinkedOmics/KIRC/KIDNEY_mRNA_FEATURES.txt")
 
-#Methy    <- read.table("~/LinkedOmics/BRCA/BRCA_Methy_FEATURES.txt")
-Methy    <- read.table("~/LinkedOmics/KIRC/KIDNEY_Methy_FEATURES.txt")
+Methy    <- read.table("~/LinkedOmics/BRCA/BRCA_Methy_FEATURES.txt")
+#Methy    <- read.table("~/LinkedOmics/KIRC/KIDNEY_Methy_FEATURES.txt")
 
-#Mut      <- read.table("~/LinkedOmics/BRCA/BRCA_Mut_FEATURES.txt")
+Mut      <- read.table("~/LinkedOmics/BRCA/BRCA_Mut_FEATURES.txt")
 
 # Outcome class
-#TARGET   <- read.table("~/LinkedOmics/BRCA/BRCA_SURVIVAL.txt")
-TARGET   <- read.table("~/LinkedOmics/KIRC/KIDNEY_SURVIVAL.txt")
+TARGET   <- read.table("~/LinkedOmics/BRCA/BRCA_SURVIVAL.txt")
+#TARGET   <- read.table("~/LinkedOmics/KIRC/KIDNEY_SURVIVAL.txt")
 
 #@FIXME -- UGLY
 # Replace NANs with mean
@@ -34,8 +34,9 @@ for (xx in na.ids){
 #-----------------------------
 
 # Perform DFNET
+DFNET_graph  <- DFNET_generate_graph_Omics(PPI, list(mRNA, Methy, Mut), TARGET, 0.95)
+DFNET_graph  <- DFNET_preprocess(DFNET_graph)
 
-DFNET_graph  <- DFNET_generate_graph_Omics(PPI, list(mRNA, Methy), TARGET, 0.95)
 
 # Make data balanced -------------------------------------------- #
 TT        <- table(unlist(TARGET))
@@ -49,8 +50,9 @@ ids2 <- which(unlist(TARGET)==class)
 
 TARGET2 <- unlist(TARGET)[c(ids1,ids2)]
 
-DFNET_graph$Feature_Matrix[[1]] <- DFNET_graph$Feature_Matrix[[1]][c(ids1,ids2),]
-DFNET_graph$Feature_Matrix[[2]] <- DFNET_graph$Feature_Matrix[[2]][c(ids1,ids2),]
+for (xx in 1:length(DFNET_graph$Feature_Matrix)){
+    DFNET_graph$Feature_Matrix[[xx]] <- DFNET_graph$Feature_Matrix[[xx]][c(ids1,ids2),]
+}
 # ---------------------------------------- #
 
 # Create TRAIN set ----------------------------------- #
@@ -58,21 +60,26 @@ DFNET_graph_train <- DFNET_graph
 ## 80% of the sample size
 smp_size  <- floor(0.80 * nrow(DFNET_graph$Feature_Matrix[[1]]))
 train_ids <- sample(seq_len(nrow(DFNET_graph$Feature_Matrix[[1]])), size = smp_size)
-DFNET_graph_train$Feature_Matrix[[1]] <- DFNET_graph$Feature_Matrix[[1]][train_ids,]
-DFNET_graph_train$Feature_Matrix[[2]] <- DFNET_graph$Feature_Matrix[[2]][train_ids,]
-
+for(xx in 1:length(DFNET_graph_train$Feature_Matrix)){
+    DFNET_graph_train$Feature_Matrix[[xx]] <- DFNET_graph$Feature_Matrix[[xx]][train_ids,]
+}
 table(TARGET2[train_ids])
 
 # Create TEST set ------------------------------------ #
 DFNET_graph_test  <- DFNET_graph
 test_ids <- (1:nrow(DFNET_graph$Feature_Matrix[[1]]))[-train_ids]
-DFNET_graph_test$Feature_Matrix[[1]] <- DFNET_graph$Feature_Matrix[[1]][test_ids,]
-DFNET_graph_test$Feature_Matrix[[2]] <- DFNET_graph$Feature_Matrix[[2]][test_ids,]
-
+for(xx in 1:length(DFNET_graph_test$Feature_Matrix)){
+    DFNET_graph_test$Feature_Matrix[[xx]] <- DFNET_graph$Feature_Matrix[[xx]][test_ids,]
+}
 table(TARGET2[test_ids])
 
-## DFNET GREEDY FOREST 
-DFNET_object <- DFNET(DFNET_graph_train, ntrees=100, niter=10, init.mtry=50)
+## DFNET GREEDY FOREST
+# default values are: 
+# ntrees = 500
+# niter = 20
+# init.mtry = 20
+
+DFNET_object <- DFNET(DFNET_graph_train, ntrees=1000, niter=1, init.mtry=50)
 
 # PREDICTION
 DFNET_pred   <- DFNET_predict(DFNET_object, DFNET_graph_test)
@@ -83,10 +90,6 @@ DFNET_perf   <- DFNET_performance(DFNET_pred, TARGET2[test_ids])
 DFNET_perf
 
 DFNET_perf$byClass
-
-# Sensitivity is more relevant because hit means death outcome
-Sensitivity  <- DFNET_perf$byClass["Sensitivity"]
-Specificity  <- DFNET_perf$byClass["Specificity"]
 
 ############################################
 # Feature Selection - Importance Measures
---
+++ R/DFNET_preprocess.R
@@ -0,0 +1,26 @@
+DFNET_preprocess <- function(DFNET_graph){
+	
+
+	range01         <- function(x){
+
+		res <- (x-min(x, na.rm=TRUE))/(max(x, na.rm=TRUE)-min(x, na.rm=TRUE))
+		if(is.infinite(res[1]) || is.na(res[1])){
+			return(x)
+		}else{
+			return(res)
+		}
+	}
+
+
+	FEAT   <- DFNET_graph$Feature_Matrix
+	
+	for(xx in 1:length(FEAT)){
+
+		FEAT[[xx]] <- as.data.frame(apply(FEAT[[xx]],2,range01))
+		colnames(FEAT[[xx]]) <- colnames(DFNET_graph$Feature_Matrix[[xx]]) 
+	}
+
+DFNET_graph$Feature_Matrix <- FEAT
+
+return(DFNET_graph)
+}
