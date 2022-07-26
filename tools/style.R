fb55c24461d0ddfdcde5629b5227223ca99a31e6
Liliana Prikler <liliana.prikler@ist.tugraz.at>
2022-07-26 11:47:51 +0200

    tools: Use qualified names.

--- tools/style.R
+++ tools/style.R
@@ -1,8 +1,7 @@
 # Usage: Rscript tools/style.R
-library(styler)
 style <- function() {
-    tidyverse_style(indent_by = 4)
+    styler::tidyverse_style(indent_by = 4)
 }
-style_dir("examples", style = style)
-style_dir("R", style = style)
-style_dir("tests", style = style)
+styler::style_dir("examples", style = style)
+styler::style_dir("R", style = style)
+styler::style_dir("tests", style = style)
